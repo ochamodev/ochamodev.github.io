c09a0ac010ab98859b139c01d68c38b5011fc1c9
Otto Chamo <cheleyotto98@gmail.com>
2024-01-14 21:50:27 -0600

    feat: Implement NavBar, Intro and Divider

---
+++ src/app/components/intro.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/app/lib/hooks";
+import { useActiveSectionContext } from "@/app/context/active-section-context";
+
+export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+
+{/**
+         * 
+         * As a software engineer with over 5 years of experience,
+         *  I have a diverse skill set and extensive knowledge in backend, frontend, and mobile development. 
+         * My expertise includes working with top-notch technologies such 
+         * as ASP.NET, Angular, Android, Swift, Kotlin, Java, and C# specifically
+         *  within the fintech industry. Currently, I am utilizing my skills to 
+         * develop cutting-edge mobile applications for clients in the US. With a proven track record
+         *  of delivering high-quality solutions and staying up-to-date with the latest industry trends, 
+         * I am confident in my ability to create impactful software that meets your business needs. Let's
+         *  connect and discuss how I can contribute to your success as a software engineer.
+         * 
+         */}
+
+
+        <span className="font-bold">Hello 👋, I'm Otto.</span> I'm a{" "}
+        <span className="font-bold">Software engineer</span> with{" "}
+        <span className="font-bold">5+ years</span> of experience, I have a diverse skill set and knowledge in backend, frontend, and mobile development.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/otto-francisco-chamo-cheley-704511150/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/ochamodev"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './components/header'
+import ActiveSectionContextProvider from './context/active-section-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
+      <div className="bg-[#9f5a51] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#574bc4] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <ActiveSectionContextProvider>
+          <Header/>
+          {children}
+        </ActiveSectionContextProvider>
+      </body>
     </html>
   )
 }
+ 
