8bba9284b7a3aad940d7ca8de0dd51bd23fa1f90
Otto Chamo <cheleyotto98@gmail.com>
2024-07-12 13:10:13 -0600

    feat: Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/header'
 import ActiveSectionContextProvider from './context/active-section-context'
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -24,6 +24,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header/>
           {children}
+          <SpeedInsights />
         </ActiveSectionContextProvider>
       </body>
     </html>
